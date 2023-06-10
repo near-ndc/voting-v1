562dba34f257929a09967337e6421329b08a1573
Robert Zaremba <robert@zaremba.ch>
2023-06-10 16:15:41 +0200

    move query functions

--- elections/src/lib.rs
+++ elections/src/lib.rs
@@ -53,18 +53,6 @@ impl Contract {
     }
 
     /**********
-     * QUERIES
-     **********/
-
-    pub fn proposals(&self) -> Vec<ProposalView> {
-        let mut proposals = Vec::with_capacity(self.prop_counter as usize);
-        for i in 1..=self.prop_counter {
-            proposals.push(self.proposals.get(&i).unwrap().to_view(i));
-        }
-        proposals
-    }
-
-    /**********
      * TRANSACTIONS
      **********/
 
--- elections/src/view.rs
+++ elections/src/view.rs
@@ -8,6 +8,17 @@ impl Contract {
     pub(crate) fn _proposal(&self, prop_id: u32) -> Proposal {
         self.proposals.get(&prop_id).expect("proposal not found")
     }
+    /**********
+     * QUERIES
+     **********/
+
+    pub fn proposals(&self) -> Vec<ProposalView> {
+        let mut proposals = Vec::with_capacity(self.prop_counter as usize);
+        for i in 1..=self.prop_counter {
+            proposals.push(self.proposals.get(&i).unwrap().to_view(i));
+        }
+        proposals
+    }
 
     pub fn proposal(&self, prop_id: u32) -> ProposalView {
         self._proposal(prop_id).to_view(prop_id)
