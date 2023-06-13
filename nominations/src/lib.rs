f169aa6d2ebac3c1749b7c55654c685167acb461
sczembor <stanislaw.czembor@gmail.com>
2023-06-13 11:06:26 +0200

    remove unecesarry import

--- nominations/src/lib.rs
+++ nominations/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedSet};
 use near_sdk::{env, near_bindgen, require, AccountId, PanicOnDefault, Promise};
 
 mod constants;
