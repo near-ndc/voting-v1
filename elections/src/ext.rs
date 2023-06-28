2be8f6bcb61b5d19baa335d341aef147f68d4987
sczembor <stanislaw.czembor@gmail.com>
2023-06-28 23:14:29 +0200

    remove unsed code

--- elections/src/ext.rs
+++ elections/src/ext.rs
@@ -6,13 +6,6 @@ use near_sdk::{ext_contract, AccountId, PromiseOrValue};
 #[allow(unused_imports)]
 use near_sdk::serde::Serialize;
 
-use crate::Vote;
-
-#[ext_contract(ext_self)]
-pub trait ExtSelf {
-    fn on_vote_verified(&mut self, prop_id: u32, user: AccountId, vote: Vote);
-}
-
 #[ext_contract(ext_sbtreg)]
 pub trait ExtSbtRegistry {
     fn is_human_call(
