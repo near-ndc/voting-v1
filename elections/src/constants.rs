df5497fc23188ec893c2e3413536ae459d380d07
Robert Zaremba <robert@zaremba.ch>
2023-07-21 00:44:13 +0200

    elections: comment and decrease vote_cost

--- elections/src/constants.rs
+++ elections/src/constants.rs
@@ -1,11 +1,11 @@
 use near_sdk::{Balance, Gas};
 
-pub const MICRO_NEAR: Balance = 1_000_000_000_000_000_000; // 1e19 yoctoNEAR
+pub const MICRO_NEAR: Balance = 1_000_000_000_000_000_000; // 1e18 yoctoNEAR
 pub const MILI_NEAR: Balance = 1_000 * MICRO_NEAR;
 
-// storage cost: 1E19 yoctoNEAR ( = 10 microN) per byte --> 1miliN 100b --> 1 NEAR 100kB
-// vote: 2 (prefix)+64bytes + 8 (proposal id) for key + 3 * 16  = 124 < 200 bytes
-pub const VOTE_COST: Balance = 2 * MILI_NEAR;
+// storage cost: 1E19 yoctoNEAR ( = 10 microN) per byte --> 1miliN per 100b --> 1 NEAR per 100kB
+// vote: max to tokenIDs: 2* (8bytes(tokenID) + prefix(1byte + 4bytes)) = 26b << 50b
+pub const VOTE_COST: Balance = MILI_NEAR / 2;
 
 pub const VOTE_GAS: Gas = Gas(70 * Gas::ONE_TERA.0);
 pub const VOTE_GAS_CALLBACK: Gas = Gas(5 * Gas::ONE_TERA.0);
