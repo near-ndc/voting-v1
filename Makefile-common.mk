0f621858a39b4a735ed7bdd306bd4edf851e5d94
Robert Zaremba <robert@zaremba.ch>
2023-07-24 14:08:00 +0200

    setup: add lint to Makefile

--- Makefile-common.mk
+++ Makefile-common.mk
@@ -11,6 +11,11 @@ build-all:
 	@RUSTFLAGS='-C link-arg=-s' cargo build --all --target wasm32-unknown-unknown --release
 	@cp ../target/wasm32-unknown-unknown/release/*.wasm ../res/
 
+lint:
+	cargo clippy  -- --no-deps
+
+lint-fix:
+	cargo clippy --fix  -- --no-deps
 
 test:
 # to test specific test run: cargo test <test name>
