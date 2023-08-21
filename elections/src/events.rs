d4e11a595c35fa638c1b4d2cd196c18d917fdebc
sczembor <43810037+sczembor@users.noreply.github.com>
2023-08-21 21:42:26 +0200

    add revoke vote event; add unit tests (#47)

--- elections/src/events.rs
+++ elections/src/events.rs
@@ -19,6 +19,13 @@ pub(crate) fn emit_vote(prop_id: u32) {
     });
 }
 
+pub(crate) fn emit_revoke_vote(prop_id: u32) {
+    emit_event(EventPayload {
+        event: "revoke_vote",
+        data: json!({ "prop_id": prop_id }),
+    })
+}
+
 #[cfg(test)]
 mod unit_tests {
     use near_sdk::{test_utils, AccountId};
@@ -32,7 +39,10 @@ mod unit_tests {
     #[test]
     fn log_vote() {
         let expected1 = r#"EVENT_JSON:{"standard":"ndc-elections","version":"1.0.0","event":"vote","data":{"prop_id":21}}"#;
+        let expected2 = r#"EVENT_JSON:{"standard":"ndc-elections","version":"1.0.0","event":"revoke_vote","data":{"prop_id":22}}"#;
         emit_vote(21);
         assert_eq!(vec![expected1], test_utils::get_logs());
+        emit_revoke_vote(22);
+        assert_eq!(vec![expected1, expected2], test_utils::get_logs());
     }
 }
