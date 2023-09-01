9a4ee0a380a58cd9b88ddd98c18c230b171096ad
Robert Zaremba <robert@zaremba.ch>
2023-09-01 14:03:57 +0200

    elections: optimize winner_by_house (#64)

--- elections/src/view.rs
+++ elections/src/view.rs
@@ -78,19 +78,14 @@ impl Contract {
             return Vec::new();
         }
 
-        let mut indexed_results: Vec<(usize, u64)> = proposal
-            .result
-            .iter()
-            .enumerate()
-            .map(|(i, &v)| (i, v))
-            .collect();
-
+        let mut indexed_results: Vec<(usize, u64)> =
+            proposal.result.into_iter().enumerate().collect();
         indexed_results.sort_by_key(|&(_, value)| std::cmp::Reverse(value));
 
         let mut winners = Vec::new();
-        for (idx, votes) in indexed_results[0..proposal.seats as usize].iter() {
-            if votes >= &proposal.min_candidate_support {
-                let candidate = proposal.candidates.get(*idx).unwrap();
+        for (idx, votes) in indexed_results.into_iter().take(proposal.seats as usize) {
+            if votes >= proposal.min_candidate_support {
+                let candidate = proposal.candidates.get(idx).unwrap();
                 winners.push(candidate.clone());
             }
         }
