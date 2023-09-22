36c22532e05ad502f21edf1976be200373850983
Robert Zaremba <robert@zaremba.ch>
2023-09-22 21:46:40 +0200

    congress: query proposals

---
+++ congress/src/view.rs
@@ -0,0 +1,37 @@
+use std::cmp::min;
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+
+use crate::*;
+
+/// This is format of output via JSON for the proposal.
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct ProposalOutput {
+    /// Id of the proposal.
+    pub id: u32,
+    #[serde(flatten)]
+    pub proposal: Proposal,
+}
+
+#[near_bindgen]
+impl Contract {
+    /// Get proposals in paginated view.
+    pub fn get_proposals(&self, from_index: u32, limit: u32) -> Vec<ProposalOutput> {
+        (from_index..min(self.prop_counter, from_index + limit))
+            .filter_map(|id| {
+                self.proposals
+                    .get(&id)
+                    .map(|proposal| ProposalOutput { id, proposal })
+            })
+            .collect()
+    }
+
+    /// Get specific proposal.
+    pub fn get_proposal(&self, id: u32) -> Option<ProposalOutput> {
+        self.proposals
+            .get(&id)
+            .map(|proposal| ProposalOutput { id, proposal })
+    }
+}
